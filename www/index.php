6266b082f03270d39742f14efefbb73ad4d9d890
Yaksat <aubekerov@list.ru>
2021-07-25 20:37:31 +0400

    first

---
+++ www/index.php
@@ -0,0 +1,13 @@
+<?php
+
+spl_autoload_register(function (string $className){
+    require_once __DIR__ . '/../src/' . str_replace('\\', '/', $className) . '.php';
+});
+
+$controller = new \MyProject\Controllers\MainController();
+
+if (!empty($_GET['name'])) {
+    $controller->sayHello($_GET['name']);
+} else {
+    $controller->main();
+}
