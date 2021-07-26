2a478d01768e3a3e529d9bc8fd8383f80690a6a9
Yaksat <aubekerov@list.ru>
2021-07-26 20:28:52 +0400

    добавил регулярные выражения

--- www/index.php
+++ www/index.php
@@ -11,3 +11,23 @@ if (!empty($_GET['name'])) {
 } else {
     $controller->main();
 }
+
+//+Информация по регулярным выражениям.
+/*
+$pattern = '/Меняем автора статьи (?P<articleId>[0-9]+) с "(.+)" на "(.+)"./';
+$string = 'Меняем автора статьи 123 с "Иван" на "Пётр".';
+
+preg_match($pattern, $string, $matches);
+var_dump($matches);
+
+$articleId = $matches['articleId'];
+$oldAuthor = $matches[2];
+$newAuthor = $matches[3];
+*/
+
+$pattern = '/\/(?P<controller>.+)\/(?P<id>[0-9]+)/';
+$url = '/post/892';
+preg_match($pattern,$url,$matches);
+$controller = $matches['controller'];
+$id = $matches['id'];
+echo $controller . $id;
