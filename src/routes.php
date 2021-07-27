c93b0c578ba238ec396b1bf4d0bf4969bbc6c7a9
Yaksat <aubekerov@list.ru>
2021-07-27 13:12:36 +0400

    add sayBye

--- src/routes.php
+++ src/routes.php
@@ -2,6 +2,7 @@
 
 return [
     '~^hello/(.*)$~' => [\MyProject\Controllers\MainController::class, 'sayHello'],
+    '~^bye/(.*)$~' => [\MyProject\Controllers\MainController::class, 'sayBye'],
     '~^$~' => [\MyProject\Controllers\MainController::class, 'main'],
 ];
 //То есть это просто массив, у которого ключи – это регулярка для адреса, а значение – это массив с двумя значениями –
