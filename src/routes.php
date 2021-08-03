aa7ef4cf275848b54d2bb00e226f364923864727
Yaksat <aubekerov@list.ru>
2021-08-03 18:45:47 +0400

    article from db

--- src/routes.php
+++ src/routes.php
@@ -3,6 +3,7 @@
 return [
     '~^hello/(.*)$~' => [\MyProject\Controllers\MainController::class, 'sayHello'],
     '~^bye/(.*)$~' => [\MyProject\Controllers\MainController::class, 'sayBye'],
+    '~^articles/(\d+)$~' => [\MyProject\Controllers\ArticlesController::class, 'view'],
     '~^$~' => [\MyProject\Controllers\MainController::class, 'main'],
 ];
 //То есть это просто массив, у которого ключи – это регулярка для адреса, а значение – это массив с двумя значениями –
