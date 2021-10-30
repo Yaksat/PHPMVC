f1369759c24e396e3547b73c5551e898cc27c499
Yaksat <aubekerov@list.ru>
2021-10-30 18:29:55 +0400

    +register

--- src/routes.php
+++ src/routes.php
@@ -5,6 +5,7 @@ return [
     '~^articles/(\d+)/edit$~' => [\MyProject\Controllers\ArticlesController::class, 'edit'],
     '~^articles/(\d+)/delete$~' => [\MyProject\Controllers\ArticlesController::class, 'delete'],
     '~^articles/add$~' => [\MyProject\Controllers\ArticlesController::class, 'add'],
+    '~^users/register$~' => [\MyProject\Controllers\UsersController::class, 'signUp'],
     '~^$~' => [\MyProject\Controllers\MainController::class, 'main'],
 ];
 //То есть это просто массив, у которого ключи – это регулярка для адреса, а значение – это массив с двумя значениями –
